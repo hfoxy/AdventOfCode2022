b91cc5c8bec1bbcd9295844a8cd7057ade3d852d
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2022-12-01 14:53:23 +0000

    Add day1 + github workflow

---
+++ days/day1/src/main/java/me/hfox/adventofcode2022/template/DayTask.java
@@ -0,0 +1,17 @@
+package me.hfox.adventofcode2022.template;
+
+import java.util.List;
+
+public class DayTask {
+
+    private final List<String> lines;
+
+    public DayTask(List<String> lines) {
+        this.lines = lines;
+    }
+
+    public Object run() {
+        return null;
+    }
+
+}
---
+++ days/day1/src/main/java/me/hfox/adventofcode2022/template/Starter.java
@@ -0,0 +1,33 @@
+package me.hfox.adventofcode2022.template;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static me.hfox.adventofcode2022.template.util.DelayedFormatter.format;
+
+public class Starter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Starter.class);
+
+    public static void main(String[] args) throws IOException {
+        String directoryName = Paths.get("").toAbsolutePath().getFileName().toString();
+        LOGGER.info("Starting Advent of Code 2022 - {}", directoryName);
+
+        File input = new File("input.txt");
+        List<String> lines = Files.readAllLines(input.toPath());
+
+        DayTask task = new DayTask(lines);
+        long start = System.nanoTime();
+        Object result = task.run();
+        long end = System.nanoTime();
+        LOGGER.info("Result: {}", result);
+        LOGGER.info("Took {}ms", format("%.03f", (end - start) / 1000000.0));
+    }
+
+}

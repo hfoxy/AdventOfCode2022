4d727627b3654b0c697a1e94a56982be2e0e626a
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2022-12-09 19:18:02 +0000

    D8P1 & D8P2

--- days/day8-part1/src/main/java/me/hfox/adventofcode2022/DayTask.java
+++ days/day8-part1/src/main/java/me/hfox/adventofcode2022/DayTask.java
@@ -1,8 +1,13 @@
 package me.hfox.adventofcode2022;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 public class DayTask {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DayTask.class);
 
     private final List<String> lines;
 
@@ -11,7 +16,101 @@ public class DayTask {
     }
 
     public Object run() {
-        return null;
+        List<String> trimmed = lines.stream().filter(v -> !v.equals("")).toList();
+
+        int count = trimmed.size();
+        int width = lines.stream().mapToInt(String::length).max().orElse(0);
+
+        int[][] heights = new int[count][width];
+        boolean[][] visible = new boolean[count][width];
+
+        int index = 0;
+        for (String line : lines) {
+            int[] lineHeights = heights[index];
+            for (int i = 0; i < line.length(); i++) {
+                char ch = line.charAt(i);
+                int height = Character.getNumericValue(ch);
+                lineHeights[i] = height;
+            }
+
+            index++;
+        }
+
+        for (int i = 0; i < heights.length; i++) {
+            LOGGER.info("Row #{}: {}", i, heights[i]);
+        }
+
+        checkRC(width, heights, visible, true, true);
+        checkRC(width, heights, visible, false, true);
+        checkRC(width, heights, visible, true, false);
+        checkRC(width, heights, visible, false, false);
+
+        checkCR(width, heights, visible, true, true);
+        checkCR(width, heights, visible, false, true);
+        checkCR(width, heights, visible, true, false);
+        checkCR(width, heights, visible, false, false);
+
+        int visibleCount = 0;
+        for (int row = 0; row < count; row++) {
+            for (int col = 0; col < width; col++) {
+                if (visible[row][col]) {
+                    visibleCount++;
+                }
+            }
+        }
+
+        return visibleCount;
+    }
+
+    public void checkRC(int width, int[][] heights, boolean[][] visible, boolean rowIncrement, boolean columnIncrement) {
+        for (int row = 0; row < heights.length; row++) {
+            int minTreeHeight = -1;
+            int rowIdx = row;
+            if (!rowIncrement) {
+                rowIdx = heights.length - 1 - row;
+            }
+
+            int[] rowHeights = heights[rowIdx];
+            boolean[] rowVisible = visible[rowIdx];
+
+            for (int col = 0; col < width; col++) {
+                int colIdx = col;
+                if (!columnIncrement) {
+                    colIdx = width - 1 - col;
+                }
+
+                int height = rowHeights[colIdx];
+                if (height > minTreeHeight) {
+                    minTreeHeight = height;
+                    rowVisible[colIdx] = true;
+                    LOGGER.info("{}/{}: Visible (RC, {}, {})", rowIdx, colIdx, rowIncrement, columnIncrement);
+                }
+            }
+        }
+    }
+
+    public void checkCR(int width, int[][] heights, boolean[][] visible, boolean rowIncrement, boolean columnIncrement) {
+        for (int col = 0; col < width; col++) {
+            int minTreeHeight = -1;
+            int colIdx = col;
+            if (!columnIncrement) {
+                colIdx = width - 1 - col;
+            }
+
+            for (int row = 0; row < heights.length; row++) {
+                int rowIdx = row;
+                if (!rowIncrement) {
+                    rowIdx = heights.length - 1 - row;
+                }
+
+                int rowHeight = heights[rowIdx][colIdx];
+                if (rowHeight > minTreeHeight) {
+                    minTreeHeight = rowHeight;
+                    visible[rowIdx][colIdx] = true;
+                    LOGGER.info("{}/{}: Visible (CR, {}, {})", rowIdx, colIdx, rowIncrement, columnIncrement);
+                }
+            }
+        }
     }
 
 }
--- days/day8-part2/src/main/java/me/hfox/adventofcode2022/DayTask.java
+++ days/day8-part2/src/main/java/me/hfox/adventofcode2022/DayTask.java
@@ -1,17 +1,139 @@
 package me.hfox.adventofcode2022;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 public class DayTask {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DayTask.class);
+
     private final List<String> lines;
+
+    private int[][] heights;
+    private int width;
+    private int height;
 
     public DayTask(List<String> lines) {
         this.lines = lines;
     }
 
     public Object run() {
-        return null;
+        List<String> trimmed = lines.stream().filter(v -> !v.equals("")).toList();
+
+        this.height = trimmed.size();
+        this.width = lines.stream().mapToInt(String::length).max().orElse(0);
+
+        this.heights = new int[height][width];
+
+        int index = 0;
+        for (String line : lines) {
+            int[] lineHeights = heights[index];
+            for (int i = 0; i < line.length(); i++) {
+                char ch = line.charAt(i);
+                int height = Character.getNumericValue(ch);
+                lineHeights[i] = height;
+            }
+
+            index++;
+        }
+
+        int bestScore = 0;
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                bestScore = Math.max(bestScore, getScore(row, col));
+            }
+        }
+
+        return bestScore;
+    }
+
+    public int getScore(int row, int column) {
+        return Direction.UP.getVisibleTrees(heights, row, column) *
+                Direction.DOWN.getVisibleTrees(heights, row, column) *
+                Direction.LEFT.getVisibleTrees(heights, row, column) *
+                Direction.RIGHT.getVisibleTrees(heights, row, column);
+    }
+
+    private enum Direction {
+        UP {
+            @Override
+            public int getVisibleTrees(int[][] heights, int rowStart, int columnStart) {
+                int visible = 0;
+                int baseHeight = heights[rowStart][columnStart];
+                for (int row = rowStart - 1; row >= 0; row--) {
+                    int height = heights[row][columnStart];
+                    if (height >= baseHeight) {
+                        visible++;
+                        break;
+                    }
+
+                    visible++;
+                }
+
+                return visible;
+            }
+        },
+        DOWN {
+            @Override
+            public int getVisibleTrees(int[][] heights, int rowStart, int columnStart) {
+                int visible = 0;
+                int baseHeight = heights[rowStart][columnStart];
+                for (int row = rowStart + 1; row < heights.length; row++) {
+                    int height = heights[row][columnStart];
+                    if (height >= baseHeight) {
+                        visible++;
+                        break;
+                    }
+
+                    visible++;
+                }
+
+                return visible;
+            }
+        },
+        LEFT {
+            @Override
+            public int getVisibleTrees(int[][] heights, int rowStart, int columnStart) {
+                int visible = 0;
+                int baseHeight = heights[rowStart][columnStart];
+                for (int col = columnStart - 1; col >= 0; col--) {
+                    int height = heights[rowStart][col];
+                    if (height >= baseHeight) {
+                        visible++;
+                        break;
+                    }
+
+                    visible++;
+                }
+
+                return visible;
+            }
+        },
+        RIGHT {
+            @Override
+            public int getVisibleTrees(int[][] heights, int rowStart, int columnStart) {
+                int width = heights[rowStart].length;
+
+                int visible = 0;
+                int baseHeight = heights[rowStart][columnStart];
+                for (int col = columnStart + 1; col < width; col++) {
+                    int height = heights[rowStart][col];
+                    if (height >= baseHeight) {
+                        visible++;
+                        break;
+                    }
+
+                    visible++;
+                }
+
+                return visible;
+            }
+        };
+
+        public abstract int getVisibleTrees(int[][] heights, int rowStart, int columnStart);
+
     }
 
 }

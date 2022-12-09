f3c27e8554eadb946ddf71f290faf25986fc1e26
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2022-12-09 15:59:32 +0000

    Add setup for days 8 and 9 - no code yet

---
+++ days/day9-part2/src/main/java/me/hfox/adventofcode2022/DayTask.java
@@ -0,0 +1,17 @@
+package me.hfox.adventofcode2022;
+
+import java.util.List;
+
+public class DayTask {
+
+    private final List<String> lines;
+
+    public DayTask(List<String> lines) {
+        this.lines = lines;
+    }
+
+    public Object run() {
+        return null;
+    }
+
+}
